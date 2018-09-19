2631107d28ecd30b3fea298ee59129ed597ebfde
Alexander Tryakin <dev@psf-group.ru>
2018-09-19 14:24:07 +0300

    Логика крона

--- backend/controllers/CronController.php
+++ backend/controllers/CronController.php
@@ -12,13 +12,38 @@ namespace backend\controllers;
 use common\helpers\YandexMarket;
 use common\models\Product;
 use yii\base\Controller;
+use yii\helpers\Json;
 
 class CronController extends Controller
 {
     public function actionYandex()
     {
 
-        $product = Product::findOne(1);
+        \Yii::$app->response->format = \yii\web\Response::FORMAT_JSON;
+
+        $result = [
+            'action' => 'cron/yandex',
+            'count' => 0,
+            'updatedId' => null,
+            'message' => 'prepare to find',
+        ];
+
+        $count = Product::find()->where(['yandex_search' => 0, 'status' => [Product::STATUS_ACTIVE, Product::STATUS_DISABLED]])->count();
+
+        $result['count'] = $count;
+
+        if (!$count) {
+            $result['message'] = 'all products already updated';
+            return $result;
+        }
+
+        $product = Product::findOne(['yandex_search' => 0, 'status' => [Product::STATUS_ACTIVE, Product::STATUS_DISABLED]]);
         $product->updateYandex();
+
+        $result['updatedId'] = $product->id;
+        $result['message'] = 'updated product ' . $product->provider_art;
+
+        return $result;
+
     }
 }
